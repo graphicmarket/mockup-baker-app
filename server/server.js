b3ed078ca7772989b5457045d69ed99434296dd0
Cesar Peña <cesarphernandez09@gmail.com>
2022-06-13 15:19:13 -0500

    Write sent buffer as collada to use in render

--- server/server.js
+++ server/server.js
@@ -1,14 +1,17 @@
 const url = require('url');
+const fs = require('fs');
 const path = require('path');
+const {join} = require('path');
 const express = require('express');
 const puppeteer = require('puppeteer')
-require('dotenv').config()
-const AWS = require('aws-sdk')
 const { fileURL } = require('./utils')
-const {ipcRenderer} = require('electron')
 
+//Config server
 const server = express();
 server.use(express.json({limit: '10gb'}))
+server.use(express.static(join(__dirname, 'public')))
+server.use(express.urlencoded({ limit: '100mb', extended: true }))
+server.use(express.static(join(__dirname, 'mockups')))
 var serverListener = undefined
 let port = '8008'
 
@@ -26,7 +29,9 @@ async function serverStatus  (data) {
     }
     return true
 }
-
+server.get('/collada',function(req,res) {
+    res.sendFile(path.resolve(__dirname, 'mockups', 'collada.dae'));
+});
 server.post('/render', async (req, res) => {
     let body = req.body
     // let response = false
@@ -53,22 +58,13 @@ server.post('/render', async (req, res) => {
 });
 
 async function renderProcess({camera, folder, scene, targetMaterialName, texture, colladaEncrypt, webGl}){
-    AWS.config.update({
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-    })
-    const s3 = new AWS.S3()
-    const params = {
-        Bucket: process.env.AWS_BUCKET,
-        Key: folder + '/collada.dae'
-    }
     try{
-        await s3.headObject(params).promise()
-        params.Expires = 30
-        const signedUrl = await s3.getSignedUrl('getObject', params)
-        console.log('signed -> ', signedUrl)
+        //Write collada
+        await fs.writeFile(`./server/mockups/collada.dae`, Buffer.from(colladaEncrypt), (err) => {
+            if (err) {throw new Error (err.message)}
+        });
         const itemData = {
-            sceneFileURL: signedUrl,
+            sceneFileURL: 'http://127.0.0.1:8008/collada',
             texture,
             targetMaterialName,
             camera,
@@ -102,6 +98,11 @@ async function renderProcess({camera, folder, scene, targetMaterialName, texture
         })
         base64 = data.split(';base64,')[1]
         await browser.close()
+
+        //Remove collada
+        await fs.unlink('./mockups/collada.dae', (err) => {
+            if (err) {throw err;}
+        });
 
         return base64
 
