d2d26c38a849423375c7548521285fd3ce6ef5e7
Martin Echavez <39717277+martinEchavez@users.noreply.github.com>
2022-07-08 16:58:37 -0500

    Files added in the previous integration are deleted

--- views/preferences/js/general.js
+++ views/preferences/js/general.js
@@ -1,9 +1,7 @@
 let aplicationPort = document.getElementById("aplicationPort");
 
 aplicationPort.addEventListener('focusout', (e) => {
-    //let aplicationPort = document.getElementById("aplicationPort");
     const { ipcRenderer } = require('electron');
-    console.log('focusout');
     e.preventDefault();
     ipcRenderer.send("setPort", aplicationPort.value)
     ipcRenderer.on("setPort", (event, data) => {
@@ -15,12 +13,8 @@ aplicationPort.addEventListener('focusout', (e) => {
 
 function getPort() {
     const { ipcRenderer } = require('electron');
-    console.log('getPort')
-    //let aplicationPort = document.getElementById("aplicationPort");
-
     ipcRenderer.send("getPort", true);
     ipcRenderer.on("sendPort", (event, data) => {
-        console.log(data)
         if(data) {
             aplicationPort.value = data
         }
